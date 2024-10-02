6ae86dc44cf859d97b57fd3f9d20a88b9b53b202
Adrián Cerezuela <72754515+adricehe11@users.noreply.github.com>
2024-10-02 17:33:39 +0200

    Update files train.py and predict.py
    Updating files trying to simulate the demo repo ones with our format

--- taed2_luminia/modeling/predict.py
+++ taed2_luminia/modeling/predict.py
@@ -1,30 +1,76 @@
 from pathlib import Path
+import mlflow
+import json
+import pickle
+import yaml
 
-import typer
-from loguru import logger
-from tqdm import tqdm
+from taed2_luminia.config import METRICS_DIR, PROCESSED_DATA_DIR
 
-from taed2_luminia.config import MODELS_DIR, PROCESSED_DATA_DIR
+# Path to the models folder
+MODELS_FOLDER_PATH = Path("models")
 
-app = typer.Typer()
+def load_validation(input_folder_path: Path):
 
+    with open(input_folder_path / "test_images.txt", "r") as file:
+        X_valid_list = file.readlines()
+    X_valid_list = [x.strip() for x in X_valid_list]  # To remove newline characters.
 
-@app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    features_path: Path = PROCESSED_DATA_DIR / "test_features.csv",
-    model_path: Path = MODELS_DIR / "model.pkl",
-    predictions_path: Path = PROCESSED_DATA_DIR / "test_predictions.csv",
-    # -----------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Performing inference for model...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Inference complete.")
-    # -----------------------------------------
+    class_names = ['prohibitory',
+    'danger',
+    'mandatory',
+    'other']
 
+    # Create a dictionary for the YAML structure
+    data = {
+        'validation': X_valid_list,
+        'nc': len(class_names),
+        'names': {i: name for i, name in enumerate(class_names)}
+    }
 
-if __name__ == "__main__":
-    app()
+    # Write the dictionary to a YAML file
+    yaml_file = 'dataset.yaml'
+    with open(yaml_file, 'w') as file:
+        yaml.dump(data, file, default_flow_style=False)
+
+    return yaml_file
+
+def evaluate_model(model_file_name, x):
+    with open(MODELS_FOLDER_PATH / model_file_name, "rb") as pickled_model:
+        traffic_model = pickle.load(pickled_model)
+
+    # Compute metrics using the model
+    valid_metrics = traffic_model.val(data=x)
+
+    return valid_metrics
+
+if __name__ == '__main__':
+    # Path to the metrics folder
+    Path("metrics").mkdir(exist_ok=True)
+    metrics_folder_path = METRICS_DIR
+
+    X_valid = load_validation(PROCESSED_DATA_DIR)
+
+    mlflow.set_experiment("traffic-sign-detection")
+
+    with mlflow.start_run():
+        metrics = evaluate_model(
+            "traffic-sign-model.pkl", X_valid)
+        
+        metrics_dict = {
+            "mAP50B": metrics['mAP50B'],  
+            "mAP50-95B": metrics['mAP50-95B'],
+            "precisionB": metrics['precisionB'], 
+            "recallB": metrics['recallB']  
+        }
+        # Log the evaluation metrics to MLflow
+        mlflow.log_metrics(metrics_dict)
+
+        # Save the evaluation metrics to a JSON file
+        with open(metrics_folder_path / "scores.json", "w") as scores_file:
+            json.dump(
+                metrics_dict,
+                scores_file,
+                indent=4,
+            )
+
+        print("Evaluation completed.")
