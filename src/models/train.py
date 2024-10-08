bd93afae4bbe9a8d055c6a0155caac464ac75198
evajivar23 <eva.jimenez.vargas@estudiantat.upc.edu>
2024-10-08 22:14:07 +0200

    update from train

--- src/models/train.py
+++ src/models/train.py
@@ -1,4 +1,3 @@
-import pickle
 from pathlib import Path
 import numpy as np
 import random
@@ -7,45 +6,54 @@ import pandas as pd
 import yaml
 from codecarbon import EmissionsTracker
 from ultralytics import YOLO
+import json
+import shutil
+import torch 
 
-from src.config import METRICS_DIR, MODELS_DIR, PROCESSED_DATA_DIR
+from src.config import PROJ_ROOT, METRICS_DIR, MODELS_DIR, PROCESSED_DATA_DIR, DATA_DIR
+
+import json
+
+
+file_path = 'settings.json'
+# Variable con el nuevo valor para 'datasets_dir'
+nuevo_datasets_dir = str(PROJ_ROOT)
+# Leer el archivo JSON
+with open(file_path, 'r') as file:
+    data = json.load(file)
+# Modificar el valor de 'datasets_dir'
+data['datasets_dir'] = nuevo_datasets_dir
+with open(file_path, 'w') as file:
+    json.dump(data, file, indent=4)
+print("El archivo JSON ha sido actualizado.")
+
 
 mlflow.set_experiment("traffic-signs")
 mlflow.sklearn.autolog(log_model_signatures=False, log_datasets=False)
 
-#config_path = os.path.join(os.getcwd(), 'settings.json')
-#os.environ['ULTRALYTICS_CONFIG'] = config_path
-## Load your settings from JSON
-#with open(config_path, 'r') as f:
-#    config = json.load(f)
-
-TRAIN_IMAGES_PATH = "data/processed/train_images.txt"
-TEST_IMAGES_PATH = "data/processed/test_images.txt"
+TRAIN_IMAGES_PATH = PROCESSED_DATA_DIR / "train_images.txt"
+TEST_IMAGES_PATH = PROCESSED_DATA_DIR / "test_images.txt"
 
 with mlflow.start_run():
     # Path of the parameters file
     params_path = Path("params.yaml")
-
-    # Read data preparation parameters
+    # Read parameters
     with open(params_path, "r", encoding="utf8") as params_file:
         try:
             params = yaml.safe_load(params_file)
-            prepare_params = params["prepare"]  # Extract prepare parameters
-            train_params = params["train"]  # Extract train parameters
+            prepare_params = params["prepare"]  
+            train_params = params["train"]
         except yaml.YAMLError as exc:
             print(exc)
-
+    
     # Read class names and number of classes from prepare parameters
-    print("prepare_params['names']:", prepare_params["names"])
-    print("Type of prepare_params['names']:", type(prepare_params["names"]))
-
     class_names = list(prepare_params["names"].values())
     nc = prepare_params["nc"]
 
     # Create the data dictionary using data lists and data preparation parameters
     data = {
-        'train': "/home/ramon/Desktop/GCED/TAED2_LuminIA/data/processed/train_images.txt", #prepare_params["train"],
-        'val': "/home/ramon/Desktop/GCED/TAED2_LuminIA/data/processed/test_images.txt", #prepare_params["test"],
+        'train': str(TRAIN_IMAGES_PATH), 
+        'val': str(TEST_IMAGES_PATH), 
         'nc': nc,
         'names': {i: name for i, name in enumerate(class_names)} 
     }
@@ -88,11 +96,19 @@ with mlflow.start_run():
     emissions = pd.read_csv(emissions_output_folder / "emissions.csv")
     emissions_metrics = emissions.iloc[-1, 4:13].to_dict()
     emissions_params = emissions.iloc[-1, 13:].to_dict()
+
     mlflow.log_params(emissions_params)
     mlflow.log_metrics(emissions_metrics)
 
     # Save the model as a pickle file
-    Path(MODELS_DIR).mkdir(exist_ok=True)
+    #ts_model.save(str(MODELS_DIR / "ts_model.pt"))
+    #ts_model.save("ts_model.pt")
 
-    with open(MODELS_DIR / "ts_model.pkl", "wb") as pickle_file:
-        pickle.dump(ts_model, pickle_file)
+    #Save the model in the models directory    
+    train_runs_path = Path("runs/detect")
+    exp_folders = sorted(train_runs_path.glob("train*"), key=lambda p: p.stat().st_mtime)
+    latest_exp_folder = exp_folders[-1]
+    default_model_path = latest_exp_folder / "weights/best.pt"
+    model_save_path = MODELS_DIR / "ts_model.pt"
+    shutil.copy(default_model_path, model_save_path)
+    print(f"Model from {default_model_path} copied to {model_save_path}")
